d806e3f5f58cf03235857f6a244838671120f0e3
greysen-png <gr10689@email.vccs.edu>
2024-10-15 16:55:54 -0400

    did all, if not most, of the assignment

--- Chapter 3 main (the real one).cpp
+++ Chapter 3 main (the real one).cpp
@@ -1,5 +1,5 @@
 //Greysen Roy-Pascoe
-//10/13/2024
+//10/15/2024
 //From the master branch
 //Ingredient adjuster project-thing
 
@@ -8,7 +8,21 @@ using namespace std;
 
 int main()
 {
+	float sugar = 1.5 / 48, butter = 1.0 / 48, flour = 2.75 / 48;
+	int cookiesUrBoyWants;
 
+	cout << "How many cookies do ya want? ";
+	cin >> cookiesUrBoyWants;
+
+	float cupsSugar = sugar * cookiesUrBoyWants;
+	float cupsButter = butter * cookiesUrBoyWants;
+	float cupsFlour = flour * cookiesUrBoyWants;
+
+	cout << "To make " << cookiesUrBoyWants << "cookie(s), you'll need:\n\n" <<
+		cupsSugar << " cups sugar\n" <<
+		cupsButter << " cups butter\n" <<
+		cupsFlour << " cups flour\n\n" <<
+		"Everything is in cups, so the decimals might be a bit large\n";
 
 	return 0;
 }
