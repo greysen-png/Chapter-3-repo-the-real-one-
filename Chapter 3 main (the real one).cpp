726cee8467b43686f3bb65024aad53c96607e53a
greysen-png <gr10689@email.vccs.edu>
2024-10-15 21:33:38 -0400

    did assignment

--- Chapter 3 main (the real one).cpp
+++ Chapter 3 main (the real one).cpp
@@ -1,11 +1,29 @@
 //Greysen Roy-Pascoe
-//10/13/2024
-//This is the main branch for the following Chp. 3 mini-projects
+//10/15/2024
+//This is the branch for the math tutor program
+//Branched from the master branch
 
 #include <iostream>
+#include <iomanip>
+#include <random>
 using namespace std;
 
 int main()
 {
+	random_device engine;
+	int min = 100, max = 999;
+	uniform_int_distribution<int> randomInt(min, max);
 
+	int firstNumber = randomInt(engine), secondNumber = randomInt(engine);
+
+	cout << "Try solving THIS!!!";
+	cout << setw(6) << firstNumber;
+	cout << "+" << setw(5) << secondNumber;
+	cout << "---------------------------";
+	
+	cin.get();
+
+	cout << firstNumber + secondNumber;
+
+	return 0;
 }
